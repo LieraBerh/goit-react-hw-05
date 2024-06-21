90fd46a49c1ce7bf2ac034a62999bf0d701fce9f
LieraBerh <lierrastern@gmail.com>
2024-06-21 16:56:51 +0200

    add vercel json

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
+import Navigation from "./components/Navigation/Navigation";
+import { Suspense, lazy } from "react";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+
+const HomePage = lazy(() => import("./pages/HomePage/HomePage"));
+const MoviesPage = lazy(() => import("./pages/MoviesPage/MoviesPage"));
 
 function App() {
   return (
     <>
-      <h1>hello</h1>
+      <Navigation />
+      <Suspense fallback={null}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/movies" element={<MoviesPage />} />
+        </Routes>
+      </Suspense>
     </>
   );
 }
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,4 +1,19 @@
+import { NavLink } from "react-router-dom";
+
 const Navigation = () => {
-  return <div></div>;
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/movies">Movies</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 export default Navigation;
